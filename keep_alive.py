5d38721f61834f52a46603cfe5c88f3df4e2d224
Yuval Rosen <yuv.rosen@gmail.com>
2022-04-04 20:45:45 +0300

    small fixes

--- keep_alive.py
+++ keep_alive.py
@@ -22,17 +22,14 @@ def create_chart():
     else:
         data = json.load(open('data.json'))
     
-
-    print(datetime.now().timestamp())
-    
-    # s = [[timestamp_to_str(k), v] for k, v in data.items()]
     s = list(map(lambda x: [convert_timezone(x[0]), x[1]], data.items()))
     df = pd.DataFrame(s, columns=['dt', 'cnt'])
     df.dt = df.dt.astype('datetime64[s]')
     df = df.set_index('dt')
     chart = json.loads(serialize(df, render_to='my-chart', output_type='json', title='Player Count'))
-    chart['series'][0]['name'] = 'Player Count'
-    chart['xAxis']['title']['text'] = 'Time (CET)'
+    if len(chart['series']) > 0:
+        chart['series'][0]['name'] = 'Player Count'
+        chart['xAxis']['title']['text'] = 'Time (CET)'
     return json.dumps(chart)
 
 
